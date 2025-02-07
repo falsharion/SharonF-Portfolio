295184bcdd402bfce5339d6f39766eb1917fefae
falsharion <falomosharl@gmail.com>
2025-02-07 15:33:33 +0100

    ui fixes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,18 @@ const { transform } = require("motion");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
+  safelist: [
+    'after:rounded-3xl',
+    "after:content-['']",
+    'after:absolute',
+    'after:inset-0',
+    'after:outline-2',
+    'after:outline',
+    'after:-outline-offset-2',
+    'after:outline-white/20',
+    'after:pointer-events-none',
+    'after:z-10',
+  ],
   darkMode: "class",
   mode: "jit",
   theme: {
